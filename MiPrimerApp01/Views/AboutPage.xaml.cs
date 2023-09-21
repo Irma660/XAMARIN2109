9085be02a264a82a752f2b87bd4aef424cde50f5
irma660 <1325455@senati.pe>
2023-09-21 15:37:53 -0500

    creando variables

--- MiPrimerApp01/Views/AboutPage.xaml.cs
+++ MiPrimerApp01/Views/AboutPage.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -7,6 +10,8 @@ namespace MiPrimerApp01.Views
 {
     public partial class AboutPage : ContentPage
     {
+        private string operador = null;
+        private double? valor1 = null, valor2 = null, resultado = null;
         public AboutPage()
         {
             InitializeComponent();
