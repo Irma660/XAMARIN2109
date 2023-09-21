213f1af245c358a03fa094c501b96df7b047e6f5
irma660 <1325455@senati.pe>
2023-09-21 17:19:36 -0500

    corrigiendo errores

--- MiPrimerApp01/Views/AboutPage.xaml.cs
+++ MiPrimerApp01/Views/AboutPage.xaml.cs
@@ -20,67 +20,103 @@ namespace MiPrimerApp01.Views
         {
             Button button = (Button)sender;
 
-            if ("0123456789".Contains(button.Text) && (valor1 == null || operador == null))
+            if (resultText.Text == "0" && button.Text != "0")
             {
-                resultText.Text = valor1.ToString() + button.Text;
-                valor1 = Convert.ToDouble(resultText.Text);
+                resultText.Text = button.Text;
             }
-            else if ("0123456789".Contains(button.Text) && (valor2 == null || operador != null))
+            else if ("0123456789".Contains(button.Text))
             {
-                resultText.Text = valor2.ToString() + button.Text;
-                valor1 = Convert.ToDouble(resultText.Text);
-            }
-            else if ("+-x/ ".Contains(button.Text) && valor1 != null)
-            {
-                operador = button.Text;
-            }
+                resultText.Text += button.Text;
 
-            else
-                calcular(valor1, valor2, operador);
-        }
-        public void calcular(double? v1,double? v2, string op)
-        {
-            switch (op)
-            {
-                case "/":
-                    if(v2 != 0)
+                if (operador == null)
+                {
+                    if (valor1 == null)
                     {
-                        resultado = v1 / v2;
-                        resultText.Text = resultado.ToString();
-                        valor1 = resultado;
-                        valor2 = null;
+                        valor1 = double.Parse(resultText.Text);
                     }
                     else
                     {
-                        resultText.Text = "NO SE PUEDE DIVIDIR";
+                        valor1 = double.Parse(resultText.Text);
                     }
-                    break;
-                case "x":
-                    resultado = v1 * v2;
-                    resultText.Text = resultado.ToString();
-                    valor1 = resultado;
-                    valor2 = null;
-                    break;
-                case "+":
-                    resultado = v1 + v2;
-                    resultText.Text = resultado.ToString();
-                    valor1 = resultado;
-                    valor2 = null;
-                    break;
-                case "-":
-                    resultado = v1 - v2;
-                    resultText.Text = resultado.ToString();
-                    valor1 = resultado;
-                    valor2 = null;
-                    break;
-                case "%":
-                    resultado = v1 / 100;
-                    resultText.Text = resultado.ToString();
-                    valor1 = resultado;
-                    valor2 = null;
-                    break;
+                }
+                else
+                {
+                    if (valor2 == null)
+                    {
+                        valor2 = double.Parse(resultText.Text);
+                    }
+                    else
+                    {
+                        valor2 = double.Parse(resultText.Text);
+                    }
+                }
+            }
+            else if ("+-x/".Contains(button.Text))
+            {
+                if (resultText.Text.EndsWith("+") || resultText.Text.EndsWith("-") || resultText.Text.EndsWith("*") || resultText.Text.EndsWith("/"))
+                {
+                    resultText.Text = resultText.Text.Remove(resultText.Text.Length - 1) + button.Text;
+                }
+                else
+                {
+                    resultText.Text += button.Text;
+                }
+                operador = button.Text;
+            }
+
+
+            else if (button.Text == "=")
+            {
+                calcular(valor1, valor2, operador);
             }
         }
+
+        public void calcular(double? v1,double? v2, string op)
+        {
+            if (v1 != null && v2 != null)
+            {
+                switch (op)
+                {
+                    case "/":
+                        if (v2 != 0)
+                        {
+                            double resultado = v1.Value / v2.Value;
+                            resultText.Text = resultado.ToString();
+                            valor1 = resultado;
+                            valor2 = null;
+                        }
+                        else
+                        {
+                            resultText.Text = "NO SE PUEDE DIVIDIR";
+                        }
+                        break;
+                    case "x":
+                        double resultadoMult = v1.Value * v2.Value;
+                        resultText.Text = resultadoMult.ToString();
+                        valor1 = resultadoMult;
+                        valor2 = null;
+                        break;
+                    case "+":
+                        double resultadoSuma = v1.Value + v2.Value;
+                        resultText.Text = resultadoSuma.ToString();
+                        valor1 = resultadoSuma;
+                        valor2 = null;
+                        break;
+                    case "-":
+                        double resultadoResta = v1.Value - v2.Value;
+                        resultText.Text = resultadoResta.ToString();
+                        valor1 = resultadoResta;
+                        valor2 = null;
+                        break;
+                    case "%":
+                        double resultadoPorcentaje = v1.Value / 100;
+                        resultText.Text = resultadoPorcentaje.ToString();
+                        valor1 = resultadoPorcentaje;
+                        valor2 = null;
+                        break;
+                }
+            }
+            }
         void OnClear(object sender, EventArgs e)
         {
             valor1 = null;
@@ -96,7 +132,14 @@ namespace MiPrimerApp01.Views
                 resultText.Text = resultText.Text.Substring(0, resultText.Text.Length - 1); // Eliminar el último carácter
             }
         }
-        
-        
+        private void OnResult(object sender, EventArgs e)
+        {
+            if (valor1 != null && valor2 != null && operador != null)
+            {
+                calcular(valor1, valor2, operador);
+            }
+        }
+
+
     }
 }
